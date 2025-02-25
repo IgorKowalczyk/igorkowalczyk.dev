5baf6cba68ce37844edb69282c9de938bf2cea58
Igor Kowalczyk <majonezexe@protonmail.com>
2025-02-25 15:39:14 +0100

    add OpenTelemetry dependencies and instrumentation setup

---
+++ instrumentation.ts
@@ -0,0 +1,11 @@
+import { registerOTel } from "@vercel/otel";
+// import { SamplingDecision } from "@opentelemetry/sdk-trace-base";
+// import { trace, Context } from "@opentelemetry/api";
+
+export function register() {
+ registerOTel({
+  serviceName: "portfolio",
+  traceExporter: "auto",
+  spanProcessors: ["auto"],
+ });
+}
