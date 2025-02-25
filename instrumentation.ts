dc0d925c56ce97d5b16a8f9b39a72f8c0955c750
Igor Kowalczyk <majonezexe@protonmail.com>
2025-02-25 16:19:05 +0100

    feat: update OpenTelemetry configuration and service name, add custom trace sampling logic

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,11 +1,27 @@
 import { registerOTel } from "@vercel/otel";
-// import { SamplingDecision } from "@opentelemetry/sdk-trace-base";
-// import { trace, Context } from "@opentelemetry/api";
+import { SamplingDecision } from "@opentelemetry/sdk-trace-base";
+import { trace, Context } from "@opentelemetry/api";
 
 export function register() {
  registerOTel({
-  serviceName: "portfolio",
-  // traceExporter: "auto",
-  // spanProcessors: ["auto"],
+  serviceName: "acme-next-app",
+  traceExporter: "auto",
+  spanProcessors: ["auto"],
+  traceSampler: {
+   shouldSample: (context: Context) => {
+    const isChecklySpan = trace.getSpan(context)?.spanContext()?.traceState?.get("checkly");
+    if (isChecklySpan) {
+     console.log("Sampling decision for Checkly span:", SamplingDecision.RECORD_AND_SAMPLED);
+     return {
+      decision: SamplingDecision.RECORD_AND_SAMPLED,
+     };
+    } else {
+     console.log("Sampling decision for non-Checkly span:", SamplingDecision.NOT_RECORD);
+     return {
+      decision: SamplingDecision.NOT_RECORD,
+     };
+    }
+   },
+  },
  });
 }
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 import { withContentlayer } from "next-contentlayer2";
 
 const nextConfig: NextConfig = {
- pageExtensions: ["jsx", "js", "ts", "tsx", "mdx"],
  env: {
   VERSION: process.env.npm_package_version,
  },
